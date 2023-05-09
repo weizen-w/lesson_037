fd6a3b0d09a8d5bd9a8b3c5c6b65569d2ca9863f
Wladimir <weizen.w@gmx.de>
2023-05-09 14:15:42 +0200

    feat: add class Man and Woman

--- src/People/Woman.java
+++ src/People/Woman.java
@@ -1,9 +1,17 @@
 package People;
 
-public class Woman {
+public class Woman extends Human {
 //  Создайте классы "Мужчина" и "Женщина", которые будут наследниками класса "Человек".
 //  Попытка вывести их на экран должна приводить к выводу строки
 //  "Мужчина по имени ..." или "Женщина по имени ..."
 //  Метод "приветствие" класса "Женщина" должен выводить строку: "Я родилась!"
 
+  public Woman(String name) {
+    super(name);
+  }
+
+  @Override
+  public String greet() {
+    return "I was born! (woman)";
+  }
 }
