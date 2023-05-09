01cc3ba343c813acc0ed90cc1f522d5b0ea4c961
Wladimir <weizen.w@gmx.de>
2023-05-09 14:16:58 +0200

    feat: add code to Main

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,16 @@
+import People.*;
+
 public class Main {
 
   public static void main(String[] args) {
-
+    Human human = new Human("Human");
+    System.out.println(human);
+    System.out.println(human.greet());
+    Human man = new Man("John");
+    System.out.println(man);
+    System.out.println(man.greet());
+    Human woman = new Woman("Jessi");
+    System.out.println(woman);
+    System.out.println(woman.greet());
   }
 }
