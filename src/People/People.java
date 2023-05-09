a05b6c301edfafcf3adc4404519ce979f3628086
Wladimir <weizen.w@gmx.de>
2023-05-09 13:37:31 +0200

    feat: add class People

--- src/People/People.java
+++ src/People/People.java
@@ -5,4 +5,18 @@ public class People {
 //  При попытке вывести объект этого класса на экран должна выводиться строка "Человек по имени ..."
 //  Добавьте в класс "Человек" метод "приветствие", который будет выводить строку: "Я родился!"
 
+  private final String name;
+
+  public People(String name) {
+    this.name = name;
+  }
+
+  public String greet() {
+    return "Я родился!";
+  }
+
+  @Override
+  public String toString() {
+    return "Человек по имени " + name;
+  }
 }
