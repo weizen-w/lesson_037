64f481c84012e94725d33aea8efe8995ffceeb1d
Wladimir <weizen.w@gmx.de>
2023-05-09 14:15:08 +0200

    feat: rename class People to class Human, fix code

--- src/People/Human.java
+++ src/People/Human.java
@@ -1,22 +1,30 @@
 package People;
 
-public class People {
+public class Human {
 //  Создайте класс "Человек", конструктор которого принимает имя.
 //  При попытке вывести объект этого класса на экран должна выводиться строка "Человек по имени ..."
 //  Добавьте в класс "Человек" метод "приветствие", который будет выводить строку: "Я родился!"
 
-  private final String name;
+  private String name;
 
-  public People(String name) {
+  public Human(String name) {
+    this.name = name;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
     this.name = name;
   }
 
   public String greet() {
-    return "Я родился!";
+    return "I was born!";
   }
 
   @Override
   public String toString() {
-    return "Человек по имени " + name;
+    return getClass().getSimpleName() + " named " + name;
   }
 }
